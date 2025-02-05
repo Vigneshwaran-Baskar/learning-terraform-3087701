c8d6918aade2a3e5275dc5497539e7099071aa04
Vigneshwaran-Baskar <107549793+Vigneshwaran-Baskar@users.noreply.github.com>
2025-02-05 14:41:28 +0530

    changes

--- main.tf
+++ main.tf
@@ -34,9 +34,9 @@ module "web_sg" {
   name  = "web_new"
 
   vpc_id = data.aws_vpc.default.id
-  ingress_rules=[http-80-tcp,https-443-tcp]
+  ingress_rules=["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules=[all-all]
+  egress_rules=["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 resource "aws_security_group" "web" {
